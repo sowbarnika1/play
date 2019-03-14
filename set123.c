3ebf497bd324f6b4bb8b03269ca26507f5111e95
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-03-14 21:04:56 +0530

    Update set123.c

--- set123.c
+++ set123.c
@@ -4,8 +4,7 @@ int main()
 {
 	char a[20];
 	scanf("%s",a);
-	int len,flag;
-	len=strlen(a);
+	int flag;
 	if(a[0]<'3' && a[1]<'10' && a[2]=='/' && a[3]<'1' && a[4]<'10' && a[5]=='/' && a[6]<'10' && a[7]<'10' && a[8]<'10' && a[9]<'10')
 	{
 		flag=1;
@@ -15,29 +14,17 @@ int main()
 		flag=0;
 		
 	}
-	if(a[0]=='3' && a[1]<'2')
+	if((a[0]=='3' && a[1]<'2')&&(a[3]=='1' && a[4]<'3'))
 	{
 	    flag=1;
-	}
-	else
-	{
-	    //flag=0;
-	}
-	if(a[3]=='1' && a[4]<'3')
-	{
-	    flag=1;
-	}
-	else
-	{
-	    //flag=0;
-	}
-	if(flag==0)
-	{
-		printf("no");
 	}
 	if(flag==1)
 	{
 		printf("yes");
+	}
+	else
+	{
+		printf("no");
 	}
 	
 	return 0;
