49488c524146e2edd5f3a7326effd460df7e8948
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-03-14 21:45:00 +0530

    Update set129.c

--- set129.c
+++ set129.c
@@ -1,16 +1,24 @@
-#include<stdio.h>
-
-int main() {
-   
-    int n,dec=0,rem,i,b=1;
-    scanf("%d",&n);
-    while(n)
-    {
-        rem=n%10;
-        dec=dec+rem*b;
-        n=n/10;
-        b=b*16;
-    }
-    printf("%d",dec);
-    return 0;
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+	char a[30];
+	scanf("%s",a);
+	int i,l,sum=0,b=1;
+	l=strlen(a);
+	for(i=l-1;i>=0;i--)
+	{
+		if(a[i]>='A' && a[i]<='F')
+		{
+			a[i]=(a[i]-65)+10;
+		}
+		else if(a[i]>='0' && a[i]<='9')
+		{
+			a[i]=a[i]-48;
+		}
+		sum=sum+(a[i]*b);
+		b=b*16;
+	}
+	printf("%d",sum);
+	return 0;
 }
