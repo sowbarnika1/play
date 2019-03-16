e4dc7ccd1f77fbde9352b0566311c2415cfada40
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-03-16 21:26:48 +0530

    Create set135.c

---
+++ set135.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100000],i,j,c=0,m;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=1;i<=n;i++)
+    {
+        c=0;
+        for(j=0;j<n;j++)
+        {
+            if(a[j]%i==0)
+            {
+                c++;
+            }
+        }
+        if(c==n)
+        {
+            m=i;
+        }
+    }
+    printf("%d",m);
+    return 0;
+}
