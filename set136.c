e81d49639d0ebb5fa299661b07c8ca0cde853741
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-03-16 21:46:43 +0530

    Create set136.c

---
+++ set136.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[100000],i,j,c=0,k,m=0,t,b[100];
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                a[j]='$';
+                c++;
+            }
+        }
+        if(c<k)
+        {
+            a[i]='$';
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]!='$')
+        {
+            b[m]=a[i];
+            m++;
+        }
+        
+    }
+    for(i=0;i<m;i++)
+    {
+        for(j=i+1;j<m;j++)
+        {
+            if(b[i]>b[j])
+            {
+                t=b[i];
+                b[i]=b[j];
+                b[j]=t;
+            }
+        }
+        printf("%d ",b[i]);
+    }
+    return 0;
+}
