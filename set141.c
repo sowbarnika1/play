1e8dbb32599db461ca22bfb0f228d818bbb74446
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-03-17 14:47:30 +0530

    Create set141.c

---
+++ set141.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() 
+{
+	int n,rem,sum=0;
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		rem=n%10;
+		if(rem%2!=0)
+		{
+			sum=sum+rem;
+		}
+		n=n/10;
+	}
+	
+	if(sum%2==0)
+	{
+		printf("E");
+	}
+	else
+	{
+		printf("O");
+	}
+	return 0;
+}
