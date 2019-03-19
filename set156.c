d8f35c52fded2132186337cc3f33aa811493c5b6
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-03-19 20:51:24 +0530

    Create set156.c

---
+++ set156.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,f=1,i,ans;
+    scanf("%d %d",&a,&b);
+    if(a-b<=5)
+    {
+    for(i=1;i<=a;i++)
+    {
+        f=f*i;
+    }
+    a=f;
+    f=1;
+    for(i=1;i<=b;i++)
+    {
+        f=f*i;
+    }
+    b=f;
+    ans=a/b;
+    printf("%d",ans);
+    }
+    return 0;
+}
