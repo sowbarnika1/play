2f83b56748f9866db20a535ce9f9126e3fc9b3f1
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-03-24 11:17:43 +0530

    Create set178.c

---
+++ set178.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char a[1000];
+    int i,n,j;
+    scanf("%s",a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]>='1' && a[i]<='9')
+        {
+            n=a[i]-48;
+            for(j=0;j<n;j++)
+            {
+                printf("%c",a[i-1]);
+            }
+        }
+    }
+    return 0;
+}
