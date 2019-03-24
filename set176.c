da7df5a5a7955b05dd5deb66714714036cbd966d
sowbarnika1 <46050816+sowbarnika1@users.noreply.github.com>
2019-03-24 10:45:37 +0530

    Create set176.c

---
+++ set176.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,f=1,gcd,i;
+    scanf("%d %d",&a,&b);
+    for(i=1;i<=a;i++)
+    {
+        f=f*i;
+    }
+    a=f;
+    f=1;
+    for(i=1;i<=b;i++)
+    {
+        f=f*i;
+    }
+    b=f;
+    for(i=1;i<=a && i<=b;i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            gcd=i;
+        }
+    }
+    printf("%d",gcd);
+    return 0;
+}
